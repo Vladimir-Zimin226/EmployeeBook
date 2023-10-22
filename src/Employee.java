83cbf7c24732e547612d19187e0dcc60e6bc76ec
Владимир <100162794+Vladimir-Korolev555@users.noreply.github.com>
2023-10-22 23:52:29 +1100

    Doing level Easy

---
+++ src/Employee.java
@@ -0,0 +1,60 @@
+public class Employee {
+
+    private static int count = 0;
+    private final String surname;
+    private final String name;
+    private final String patronimic;
+    private String departament;
+    private int salary;
+    private int id;
+
+
+    public Employee(String surname, String name, String patronimic, String departament, int salary) {
+        this.surname = surname;
+        this.name = name;
+        this.patronimic = patronimic;
+        this.departament = departament;
+        this.salary = salary;
+        count++;
+        id = count;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPatronimic() {
+        return patronimic;
+    }
+
+    public String getDepartament() {
+        return departament;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setDepartament(String departament) {
+        this.departament = departament;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Сотрудник " + id + " - " +
+                "Фамилия: " + surname +
+                " Имя: " + name +
+                " Отчетство: " + patronimic +
+                ". Работает в отделе: " + departament +
+                ", Заработная плата: " + salary + " руб.";
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import java.util.Arrays;
+
 public class Main {
+
+    public static void printAllEmployees(String[] employee) {
+        for (int i = 0; i < employee.length; i++) System.out.println(employee[i]);
+    }
+
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        String[] employee = new String[10];
+
+        employee[0] = String.valueOf(new Employee("Иванов", "Иван", "Иванович", "Один", 100_000));
+        employee[1] = String.valueOf(new Employee("Петров", "Петр", "Петрович", "Один", 100_000));
+        employee[2] = String.valueOf(new Employee("Сидоров", "Сидор", "Сидорович", "Два", 120_000));
+        employee[3] = String.valueOf(new Employee("Свиридов", "Свирид", "Свиридович", "Два", 120_000));
+        employee[4] = String.valueOf(new Employee("Павлов", "Павел", "Павлович", "Три", 140_000));
+        employee[5] = String.valueOf(new Employee("Егоров", "Егор", "Егорович", "Три", 140_000));
+        employee[6] = String.valueOf(new Employee("Александров", "Александр", "Александрович", "Три", 140_000));
+        employee[7] = String.valueOf(new Employee("Дмитриев", "Дмитрий", "Дмитриевич", "Четыре", 170_000));
+        employee[8] = String.valueOf(new Employee("Сергеев", "Сергей", "Сергеевич", "Четыре", 170_000));
+        employee[9] = String.valueOf(new Employee("Владимиров", "Владимир", "Владимирович", "Пять", 200_000));
+
+        printAllEmployees(employee);
+
     }
 }
